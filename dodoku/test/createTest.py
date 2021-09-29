2ebaad1fba702e53b5f090dc3016f49e49f00815
Jay Pankey <pankey.jay@gmail.com>
2021-09-29 16:43:38 -0500

    Red Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -1,5 +1,29 @@
 from unittest import TestCase
 import dodoku.create as create 
 
+
 class CreateTest(TestCase):
-        pass
+
+    # 100 create
+    #    Desired level of confidence:    grid creation
+    #    Input-output Analysis
+    #      inputs: value -> numeric; .GE. 0 and .LT. 100; mandatory, unvalidated
+    #      outputs: 
+    #         normal:  dictionary with "grid", "integrity", and "status"
+    #         abnormal: error
+    #         side effects:  none
+    
+    
+    #  Happy path tests
+    #        test 010: nominal value that has one digit:  '9'
+    #            result: '9'
+    #        
+    
+    
+    def test100_010_CreateWithThreeParams(self):
+        expectedResult = ['grid','status', 'integrity']
+        parms = {'op': 'create', 'level' : '1'}
+        actualResult = create._create(parms).keys()
+        self.assertDictEqual(expectedResult, actualResult)
+
+        
