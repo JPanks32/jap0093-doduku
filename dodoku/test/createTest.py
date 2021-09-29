a0ed12f1cbe356be8e2d6f21ea70b74abcb3fa17
Jay Pankey <pankey.jay@gmail.com>
2021-09-29 17:39:07 -0500

    Red Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -69,6 +69,36 @@ class CreateTest(TestCase):
         print(actualResult)
         self.assertEqual(expectedResult['grid'], actualResult['grid'])
         
+    def test_Create_030_Lvl2status(self):
+        expectedResult = {}
+        grid = [
+            0,-6,0,0,0,0,0,-5,-9,
+            -9,-3,0,-4,-8,0,0,0,0,
+            0,0,0,0,0,-7,-3,0,0,
+            0,-5,0,0,-1,0,0,-4,-6,
+            0,0,0,0,0,-6,0,-9,0,
+            0,-8,-1,-2,0,0,0,0,0,
+            0,0,0,0,-7,0,0,0,0,
+            0,0,0,0,-5,0,-8,0,-4,
+            0,0,-1,0,0,0,-7,0,0,
+            -6,0,-2,0,-9,0,0,0,0,
+            0,0,0,0,-5,0,0,0,0,
+            0,0,0,0,0,-9,-5,-3,0,
+            0,-7,0,-4,0,0,0,0,0,
+            -5,-8,0,0,-1,0,0,-9,0,
+            0,0,-2,-1,0,0,0,0,0,
+            0,0,0,0,-9,-8,0,-6,-1,
+            -6,-1,0,0,0,0,0,-7,0
+            ]
+        status ='ok'
+        integrity = '6fcd71ef7722e7573d2f607a35cfa48f72b03c4cea135ac31f7ef73a58e50a8a'
+        expectedResult["grid"] = grid
+        expectedResult["status"] = status
+        expectedResult["integrity"] = integrity
+        parms = {'op': 'create', 'level' : '2'}
+        actualResult = create._create(parms)
+        print(actualResult)
+        self.assertEqual(expectedResult['status'], actualResult['status'])
         
         
         #self.assertEqual(expectedResult['status'], actualResult['status'])
