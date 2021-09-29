afacd7bc114e46f58502bc59281de99c04ad43fb
Jay Pankey <pankey.jay@gmail.com>
2021-09-29 17:24:16 -0500

    Red Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import dodoku.create as create 
+from _operator import truediv
 
 
 class CreateTest(TestCase):
@@ -15,17 +16,57 @@ class CreateTest(TestCase):
     
     
     #  Happy path tests
-    #        test 010: nominal value that has one digit:  '9'
-    #            result: '9'
+    #        test 010: returns dictionary with keys
+    #            result: ['grid', 'status', 'integrity']
+    #        test 020: returns level 2
+    #            result: grid = [0,-6,0,0,0,0,0,-5,-9,-9,-3,0,-4,-8,0,0,0,0,0,0,0,0,0,-7,-3,0,0,0,-5,0,0,-1,0,0,-4,-6,0,0,0,0,0,-
+    #                                6,0,-9,0,0,-8,-1,-2,0,0,0,0,0,0,0,0,0,-7,0,0,0,0,0,0,0,0,-5,0,-8,0,-4,0,0,-1,0,0,0,-7,0,0,-6,0,-2,0,-
+    #                                9,0,0,0,0,0,0,0,0,-5,0,0,0,0,0,0,0,0,0,-9,-5,-3,0,0,-7,0,-4,0,0,0,0,0,-5,-8,0,0,-1,0,0,-9,0,0,0,-2,-
+    #                                1,0,0,0,0,0,0,0,0,0,-9,-8,0,-6,-1,-6,-1,0,0,0,0,0,-7,0] AND status = 'ok' AND the length of the
+    #                    value of integrity = 8 AND the value of integrity is a substring of
+    #                        6fcd71ef7722e7573d2f607a35cfa48f72b03c4cea135ac31f7ef73a58e50a8a
     #        
     
     
-    def test100_010_CreateWithThreeParams(self):
+    def test_Create_010_ThreeKeys(self):
         expectedResult = {'grid': '','status': '', 'integrity': ''}
         expectedResult = expectedResult.keys()
         parms = {'op': 'create', 'level' : '1'}
         actualResult = create._create(parms)
         actualResult = actualResult.keys()
         self.assertEqual(expectedResult, actualResult)
+        
+    def test_Create_020_ShouldReturnMyUsername(self):
+        expectedResult = {}
+        grid = [
+            0,-6,0,0,0,0,0,-5,-9,
+            -9,-3,0,-4,-8,0,0,0,0,
+            0,0,0,0,0,-7,-3,0,0,
+            0,-5,0,0,-1,0,0,-4,-6,
+            0,0,0,0,0,-6,0,-9,0,
+            0,-8,-1,-2,0,0,0,0,0,
+            0,0,0,0,-7,0,0,0,0,
+            0,0,0,0,-5,0,-8,0,-4,
+            0,0,-1,0,0,0,-7,0,0,
+            -6,0,-2,0,-9,0,0,0,0,
+            0,0,0,0,-5,0,0,0,0,
+            0,0,0,0,0,-9,-5,-3,0,
+            0,-7,0,-4,0,0,0,0,0,
+            -5,-8,0,0,-1,0,0,-9,0,
+            0,0,-2,-1,0,0,0,0,0,
+            0,0,0,0,-9,-8,0,-6,-1,
+            -6,-1,0,0,0,0,0,-7,0
+            ]
+        status ='ok'
+        integrity = '6fcd71ef7722e7573d2f607a35cfa48f72b03c4cea135ac31f7ef73a58e50a8a'
+        expectedResult["grid"] = grid
+        expectedResult["status"] = status
+        expectedResult["integrity"] = integrity
+        parms = {'op': 'create', 'level' : '2'}
+        actualResult = create._create(parms)
+        self.assertEqual(expectedResult['grid'], actualResult['grid'])
+        self.assertEqual(expectedResult['status'], actualResult['status'])
+        self.assertIn(actualResult['integrity'], expectedResult['integrity'])
+        
 
         
