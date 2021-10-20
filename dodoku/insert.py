e29d0214fd2cc5319785e5ecc86ed07b24989ab9
Jay Pankey <pankey.jay@gmail.com>
2021-10-20 17:16:21 -0500

    Green Light |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -2,5 +2,40 @@ def _insert(parms):
     result = {'status': 'insert stub'}
     return result
 
+def _find_location(loc):
+    r = 1
+    c = 1
+    return r, c
+
 def _check_input(parms):
-    return False
+    result = True
+    try:
+        _find_location(parms['cell'])
+    except:
+        result = False
+    try:
+        if int(parms['value']) < 1 or int(parms['value']) > 9:
+            result = False
+    except:
+        try:
+            x = parms['value']
+            if type(x) is not int:
+                result = False
+        except:
+            continue
+    try:
+        if len(parms['grid']) != 153:
+            result = False
+        for val in parms['grid']:
+            if val < -9 or val > 9 or type(val) is not int:
+                result = False
+                break
+    except:
+        result = False
+    try:
+        if len(parms['integrity']) != 8 or type(parms['integrity']) is not str:
+            result = False
+    except:
+        result = False
+    return result
+
