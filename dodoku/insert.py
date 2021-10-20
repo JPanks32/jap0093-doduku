7c165745de560be945701915788ea388a96d28df
Jay Pankey <pankey.jay@gmail.com>
2021-10-20 16:39:15 -0500

    Red Light |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -1,3 +1,6 @@
 def _insert(parms):
     result = {'status': 'insert stub'}
     return result
+
+def _check_input(parms):
+    return False
--- dodoku/test/insertTest.py
+++ dodoku/test/insertTest.py
@@ -2,4 +2,39 @@ from unittest import TestCase
 import dodoku.insert as insert 
 
 class InsertTest(TestCase):
-        pass
+            # 100 Insert
+    #    Desired level of confidence:    grid value insertion
+    #    Input-output Analysis
+    #      inputs: dictionary with "cell", "value", "grid", and "integrity"; .GE. 0 and .LT. 100; mandatory, unvalidated
+    #      outputs: 
+    #         normal:  dictionary with "grid", "integrity", and "status"
+    #         abnormal: error
+    #         side effects:  none
+    
+    #  Happy path tests
+    #        test 010: check_integrity returns true
+    #            result: true
+    #   
+    
+    def test_Create_010_ThreeKeys(self):
+        expectedResult = True
+        grid = [0,-2,0,0,-1,0,0,-4,0,
+                -8,0,-1,-9,0,0,0,0,-5,
+                0,0,0,0,-3,0,0,-1,0,
+                0,-3,0,0,0,0,-4,0,-6,
+                -5,0,-9,0,0,0,0,0,-7,
+                0,0,0,0,0,0,-2,-8,0,
+                -2,0,0,-6,0,0,0,0,0,
+                0,-1,-4,0,-6,0,0,0,-6,
+                0,0,-3,0,0,0,-2,0,0,
+                -1,0,-9,0,-4,0,-5,-7,0,
+                0,0,0,0,0,-7,0,0,-5,
+                0,0,-6,0,0,0,0,-9,0,
+                -2,0,0,0,0,0,-4,0,-8,
+                -7,0,-9,0,0,0,0,0,0,
+                0,-5,0,0,-9,0,0,0,0,
+                -4,0,0,-6,0,-3,-9,0,0,
+                0,-6,0,0,-5,0,0,-3,-1]
+        parms = {'value': '3', 'cell' : 'r7c9', 'grid': grid , 'integrity' : '93d46bcb'}
+        actualResult = insert._check_input(parms)
+        self.assertEqual(expectedResult, actualResult)
