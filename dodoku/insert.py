d890e02e13c44a59be8e0607378c62013bb4e2c0
Jay Pankey <pankey.jay@gmail.com>
2021-10-20 19:42:17 -0500

    Red Light |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -67,6 +67,83 @@ def _column_major(grid):
             result.append(num)
     return result
 
+def _organize(grid):
+    col_maj = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    row_maj = [[0]* 9,[0]* 9,[0]* 9,[0]* 9,[0]* 9,[0]* 9,[0]* 15,[0]* 15,[0]* 15,[0]* 9,[0]* 9,[0]* 9,[0]* 9,[0]* 9,[0]* 9]
+    block_row_maj = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    result = []
+    rowCount = 0
+    colCount = 0
+    for i in range(0, 54):
+        rowCount = int((i -(i % 9)) / 9)
+        row_maj[rowCount][colCount] = grid[i]
+        col_maj[colCount].append(grid[i])
+        
+        
+        if rowCount < 3:
+            if colCount < 3:
+                block_row_maj[0].append(grid[i])
+            elif colCount < 6:
+                block_row_maj[1].append(grid[i])
+            elif colCount < 9:
+                block_row_maj[2].append(grid[i])
+        elif rowCount < 6:
+            if colCount < 3:
+                block_row_maj[3].append(grid[i])
+            elif colCount < 6:
+                block_row_maj[4].append(grid[i])
+            elif colCount < 9:
+                block_row_maj[5].append(grid[i])
+        colCount += 1
+        
+        if colCount > 8:
+            colCount = 0
+    colCount = 0
+    for i in range(54,99):
+        rowCount = int((i-54 - ((i-54) % 15)) / 15) + 6
+        row_maj[rowCount][colCount] = grid[i]
+        col_maj[colCount].append(grid[i])
+        if rowCount < 9:
+            if colCount < 3:
+                block_row_maj[6].append(grid[i])
+            elif colCount < 6:
+                block_row_maj[7].append(grid[i])
+            elif colCount < 9:
+                block_row_maj[8].append(grid[i])
+            elif colCount < 12:
+                block_row_maj[9].append(grid[i])
+            elif colCount < 15:
+                block_row_maj[10].append(grid[i])
+                
+        colCount += 1      
+        if colCount > 14:
+            colCount = 0
+            
+    colCount = 6
+    for i in range(99, 153):
+        rowCount = int((i-99 - ((i-99) % 9)) / 9) + 9
+        row_maj[rowCount][colCount - 6] = grid[i]
+        col_maj[colCount].append(grid[i])
+        
+        if rowCount < 12:
+            if colCount < 9:
+                block_row_maj[11].append(grid[i])
+            elif colCount < 12:
+                block_row_maj[12].append(grid[i])
+            elif colCount < 15:
+                block_row_maj[13].append(grid[i])
+        elif rowCount < 15:
+            if colCount < 9:
+                block_row_maj[14].append(grid[i])
+            elif colCount < 12:
+                block_row_maj[15].append(grid[i])
+            elif colCount < 15:
+                block_row_maj[16].append(grid[i])
+        colCount += 1
+        if colCount > 14:
+            colCount = 6
+
+    return block_row_maj, col_maj, row_maj
 #It concatinates all of the strings in the array into one string
 def _concat_columns(cols):
     result = ""
